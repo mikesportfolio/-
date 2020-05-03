24ade7960ef2a029aaa5ff086dde08809c61c4a1
DNA-MAC <michaelintellect2007@gmail.com>
2020-05-03 17:45:15 +0200

    react new portfolio page commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.min.css'
+import './css/animate.css'
+import './css/responsive.css'
+import './css/jasny-bootstrap.min.css'
+import './css/style.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
