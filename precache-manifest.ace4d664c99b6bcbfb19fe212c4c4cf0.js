297abb497977df051f7ec7232086b0a18c6f180e
mikesportfolio <michaelintellect2007@gmail.com>
2020-05-22 23:02:08 +0200

    Updates

---
+++ precache-manifest.ace4d664c99b6bcbfb19fe212c4c4cf0.js
@@ -0,0 +1,262 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57a63ef0ad63ae463c0b4c9eb018424",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "7f2af2dcc58ddb2e5bc8",
+    "url": "/profile/static/css/2.749e4313.chunk.css"
+  },
+  {
+    "revision": "92bc4658c0df119c244c",
+    "url": "/profile/static/css/main.0f22e8e6.chunk.css"
+  },
+  {
+    "revision": "7f2af2dcc58ddb2e5bc8",
+    "url": "/profile/static/js/2.4efaa254.chunk.js"
+  },
+  {
+    "revision": "40be5ea31f0ae9d6ed3e05bd4f0b277c",
+    "url": "/profile/static/js/2.4efaa254.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92bc4658c0df119c244c",
+    "url": "/profile/static/js/main.6f09e004.chunk.js"
+  },
+  {
+    "revision": "f847fcd93b30426c2cf6",
+    "url": "/profile/static/js/runtime-main.d59f4c94.js"
+  },
+  {
+    "revision": "8711cfc4d1eca36fa6457ca70caf3224",
+    "url": "/profile/static/media/Angular_ezgif.com-resize.8711cfc4.gif"
+  },
+  {
+    "revision": "3f60bf902954cc89cfd9277d6e02e147",
+    "url": "/profile/static/media/Koha_1.3f60bf90.JPG"
+  },
+  {
+    "revision": "468e91af4bc571bfd6286425c17fd6f0",
+    "url": "/profile/static/media/Koha_2.468e91af.JPG"
+  },
+  {
+    "revision": "56c845ff9940b5506fdda457a3258618",
+    "url": "/profile/static/media/Koha_3.56c845ff.JPG"
+  },
+  {
+    "revision": "6d8fa254d14cb8319b9010f24a631fc9",
+    "url": "/profile/static/media/Screenshot 2020-05-18 at 20.08.34.6d8fa254.png"
+  },
+  {
+    "revision": "b88df3e308e89e939f40e04887bd8f65",
+    "url": "/profile/static/media/Screenshot 2020-05-18 at 20.12.56.b88df3e3.png"
+  },
+  {
+    "revision": "0445fa509e06c56415e843866b9af959",
+    "url": "/profile/static/media/Screenshot 2020-05-18 at 20.13.20.0445fa50.png"
+  },
+  {
+    "revision": "e87ebc82c69d4b9d43530cca166185f1",
+    "url": "/profile/static/media/Screenshot 2020-05-18 at 20.14.21.e87ebc82.png"
+  },
+  {
+    "revision": "64c36792f8021b1b254cab9aa9e843ac",
+    "url": "/profile/static/media/Screenshot 2020-05-19 at 22.14.24.64c36792.png"
+  },
+  {
+    "revision": "34d4d190398d4645585e1eb6f9f28744",
+    "url": "/profile/static/media/Screenshot 2020-05-19 at 22.15.28.34d4d190.png"
+  },
+  {
+    "revision": "29c739f51cab95864dda2f16713032d0",
+    "url": "/profile/static/media/Screenshot 2020-05-19 at 22.16.59.29c739f5.png"
+  },
+  {
+    "revision": "53d6ae05aae1d06f9ecc64d61096296c",
+    "url": "/profile/static/media/Screenshot 2020-05-19 at 22.19.20.53d6ae05.png"
+  },
+  {
+    "revision": "feb2bf8b6c0634d35c0f8e9850a83f74",
+    "url": "/profile/static/media/Screenshot 2020-05-19 at 22.20.01.feb2bf8b.png"
+  },
+  {
+    "revision": "f0a099f71955a0587975de9ad103a9fd",
+    "url": "/profile/static/media/Webp.net-compress-image copy.f0a099f7.jpg"
+  },
+  {
+    "revision": "156b78edd1f734131670559c1141d1c3",
+    "url": "/profile/static/media/activemq_stack_220x234_IQS_icon.156b78ed.ico"
+  },
+  {
+    "revision": "babf98c9227de63e977e325554f567b9",
+    "url": "/profile/static/media/aws_logo_smile_1200x630_Ids_icon.babf98c9.ico"
+  },
+  {
+    "revision": "500f9614b6326ed0134a549c9522bef8",
+    "url": "/profile/static/media/bg1.500f9614.jpg"
+  },
+  {
+    "revision": "6b6ddc527bfafb0bb8a6d67c650d256f",
+    "url": "/profile/static/media/bg2.6b6ddc52.jpg"
+  },
+  {
+    "revision": "3437790706adac28d73d235a1020f28b",
+    "url": "/profile/static/media/bg3.34377907.jpg"
+  },
+  {
+    "revision": "dffd90606688ed9004f0d8544ea5371f",
+    "url": "/profile/static/media/bg4.dffd9060.jpg"
+  },
+  {
+    "revision": "2709d666f20896bf1eddbd88472f227f",
+    "url": "/profile/static/media/c_programing_language_Jvr_icon.2709d666.ico"
+  },
+  {
+    "revision": "aab3d2529ae1919783182c3d43af5f80",
+    "url": "/profile/static/media/camunda_uRv_icon.aab3d252.ico"
+  },
+  {
+    "revision": "511ddf9d9168f15be4a31f14450e0c9e",
+    "url": "/profile/static/media/dspace_1.511ddf9d.JPG"
+  },
+  {
+    "revision": "dd3d0bb36c80ca91e2b58aa31697f0f5",
+    "url": "/profile/static/media/dspace_2.dd3d0bb3.JPG"
+  },
+  {
+    "revision": "f088289085c49a94d8d05bc34cff0661",
+    "url": "/profile/static/media/dspace_3.f0882890.JPG"
+  },
+  {
+    "revision": "6dd0063b885fea23ada1491ec3128a79",
+    "url": "/profile/static/media/dspace_4.6dd0063b.JPG"
+  },
+  {
+    "revision": "30ca96fe3476e04818ec64fff5643384",
+    "url": "/profile/static/media/excel1Image.30ca96fe.jpg"
+  },
+  {
+    "revision": "0d749badb7976681fa2a2d875d23a377",
+    "url": "/profile/static/media/excelvb_wQj_icon.0d749bad.ico"
+  },
+  {
+    "revision": "d294c3e0952140524c0a5145e808d9e3",
+    "url": "/profile/static/media/ezgif.com-resize.d294c3e0.gif"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/profile/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/profile/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/profile/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/profile/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/profile/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c62da92762e4988444a1948bddf41824",
+    "url": "/profile/static/media/fuseesb_vzW_icon.c62da927.ico"
+  },
+  {
+    "revision": "68ed1dac06bf0409c18ae7bc62889170",
+    "url": "/profile/static/media/glyphicons-halflings-regular.68ed1dac.woff"
+  },
+  {
+    "revision": "7ad17c6085dee9a33787bac28fb23d46",
+    "url": "/profile/static/media/glyphicons-halflings-regular.7ad17c60.eot"
+  },
+  {
+    "revision": "e49d52e74b7689a0727def99da31f3eb",
+    "url": "/profile/static/media/glyphicons-halflings-regular.e49d52e7.ttf"
+  },
+  {
+    "revision": "ff423a4251cf2986555523dfe315c42b",
+    "url": "/profile/static/media/glyphicons-halflings-regular.ff423a42.svg"
+  },
+  {
+    "revision": "f608cc6604e327583af8ad51e92890eb",
+    "url": "/profile/static/media/groovey_rpw_icon.f608cc66.ico"
+  },
+  {
+    "revision": "9eaa6cadb11acf6122ffab7d6f367960",
+    "url": "/profile/static/media/hibernate_ZZp_icon.9eaa6cad.ico"
+  },
+  {
+    "revision": "1558dc7cb861e9ab05fecc19e43e4afd",
+    "url": "/profile/static/media/htmlcssimg_9Wf_icon.1558dc7c.ico"
+  },
+  {
+    "revision": "492565339911622fe19856445ce63821",
+    "url": "/profile/static/media/javaicon_PFz_icon.49256533.ico"
+  },
+  {
+    "revision": "8b9ef88398ad92f0c763ed005feee02f",
+    "url": "/profile/static/media/javascriptimage_Tq4_icon.8b9ef883.ico"
+  },
+  {
+    "revision": "e5d00d1a347a1c3f07ea779ed64e2519",
+    "url": "/profile/static/media/microservices_Le6_icon.e5d00d1a.ico"
+  },
+  {
+    "revision": "351d702214acce2c1293aca4cdc55a47",
+    "url": "/profile/static/media/new_excel-1.351d7022.png"
+  },
+  {
+    "revision": "52f04a337a4f3497faee2bd49ae0d530",
+    "url": "/profile/static/media/new_excel-2.52f04a33.png"
+  },
+  {
+    "revision": "37bc25cd779f353c9898acee5d163a6a",
+    "url": "/profile/static/media/new_excel-3.37bc25cd.png"
+  },
+  {
+    "revision": "0edbbd069fd18f5cd9e342814acc639d",
+    "url": "/profile/static/media/new_excel-4.0edbbd06.png"
+  },
+  {
+    "revision": "8de42074c8b34954253eb459125b7a4b",
+    "url": "/profile/static/media/reduximg_FWv_icon.8de42074.ico"
+  },
+  {
+    "revision": "234e8560060d6e628cdfcb14cd3db5a7",
+    "url": "/profile/static/media/restapi_KZy_icon.234e8560.ico"
+  },
+  {
+    "revision": "2e7fc7f64a89fe1a351e94e867d185bb",
+    "url": "/profile/static/media/screenshot_2020_05_1_Qxk47.2e7fc7f6.jpg"
+  },
+  {
+    "revision": "579af06c633603f170e913fcd4a5d3fa",
+    "url": "/profile/static/media/screenshot_2020_05_1_cfI2U.579af06c.jpg"
+  },
+  {
+    "revision": "16039a5e26501c1a984e0a717cf7a506",
+    "url": "/profile/static/media/springboot_3Tw_icon.16039a5e.ico"
+  },
+  {
+    "revision": "79113102721b4b2b023cc1e43250810a",
+    "url": "/profile/static/media/springicon_UFY_icon.79113102.ico"
+  },
+  {
+    "revision": "b464b65ab25cddff86f556aeffcdc3c4",
+    "url": "/profile/static/media/sqlimg_O45_icon.b464b65a.ico"
+  },
+  {
+    "revision": "fa8e59e65ce8fe4dda2ea827b1bc8583",
+    "url": "/profile/static/media/typescriptimg_HSp_icon.fa8e59e6.ico"
+  },
+  {
+    "revision": "6bc13ae1b3311c8f691a7295dfaba98b",
+    "url": "/profile/static/media/vue_resized.6bc13ae1.gif"
+  }
+]);
